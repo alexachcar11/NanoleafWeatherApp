d37ccff5fcf39ac01a24e8302cd786a14ab7056f
Alexander Achcar <53192737+alexachcar11@users.noreply.github.com>
2022-01-20 00:44:46 -0500

    Update weather.java
    Made small changes regarding class structure

--- weather.java
+++ weather.java
@@ -10,15 +10,15 @@ import org.json.*;
 
 // Extracting the weather using OpenWeatherMap
 
-public class weather { 
+public static class Weather { 
 
-	public static void main(String[] args) throws Exception{ 
+	public static Double getTemp() throws Exception { 
 
 		try{ 
 
 			StringBuilder str = new StringBuilder();
 
-			URL url = new URL("https://api.openweathermap.org/data/2.5/weather?q=Montreal&appid={insert-api-key}");
+			URL url = new URL("https://api.openweathermap.org/data/2.5/weather?q=Montreal&appid=69df68df2814a54fc3200394b7982ddb");
 
 			HttpURLConnection connection = (HttpURLConnection)url.openConnection();
 
@@ -45,14 +45,54 @@ public class weather {
 			// stores the celsius temperature in a Double variable
 			Double tempInC = tempInK - 273.15;
 			
-			System.out.println(tempInC);
+			return(tempInC);
 			
 
 		}catch (IOException e){
-			System.out.println(e.getMessage());
+			throw new Exception(e.getMessage());
 		}
-		
 
 	}
+
+	// public static void main(String[] args) throws Exception{ 
+
+		// try{ 
+
+		// 	StringBuilder str = new StringBuilder();
+
+		// 	URL url = new URL("https://api.openweathermap.org/data/2.5/weather?q=Montreal&appid=69df68df2814a54fc3200394b7982ddb");
+
+		// 	HttpURLConnection connection = (HttpURLConnection)url.openConnection();
+
+		// 	BufferedReader reader = new BufferedReader((new InputStreamReader(connection.getInputStream())));
+
+		// 	String line;
+
+		// 	while((line = reader.readLine()) != null) {
+		// 		str.append(line);
+		// 	}
+
+		// 	reader.close();
+
+			
+		// 	// System.out.println(str); // <-- prints out all of the information provided by openweathermap
+
+		// 	JSONObject obj = new JSONObject(str.toString());
+
+		// 	// stores the kelvin temperature in a String variable
+		// 	String temp = obj.getJSONObject("main").get("temp").toString();;
+
+		// 	Double tempInK = Double.parseDouble(temp);
+		
+		// 	// stores the celsius temperature in a Double variable
+		// 	Double tempInC = tempInK - 273.15;
+			
+		// 	System.out.println(tempInC);
+			
+
+		// }catch (IOException e){
+		// 	System.out.println(e.getMessage());
+		// }
+	// }
 
 }
